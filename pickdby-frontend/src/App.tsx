6402544fcba936f8a5f250c5216392055f753109
Riccardo Varenna <riccardovarenna@gmail.com>
2025-07-17 17:59:47 +0200

    First Hello World Deploy

---
+++ pickdby-frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+      <h1>Hello World from Pickdby</h1>
+      <p>Frontend is working 🎉</p>
+    </div>
+  );
+}
+
+export default App;
